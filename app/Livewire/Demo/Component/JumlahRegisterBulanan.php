ea49cc588cfea4ce482377298a533382e03b057b
vandewa <devandewananta@gmail.com>
2024-07-11 11:54:22 +0700

    bug uat pertama

--- app/Livewire/Demo/Component/JumlahRegisterBulanan.php
+++ app/Livewire/Demo/Component/JumlahRegisterBulanan.php
@@ -40,54 +40,54 @@ class JumlahRegisterBulanan extends Component
     protected $index = -1;
     public function render()
     {
-        // $query = DB::select("
-        // WITH all_months AS (
-        //     SELECT generate_series(
-        //         '" . $this->tahun . "-01-01'::date,
-        //         '" . $this->tahun . "-12-31'::date,
-        //         interval '1 month'
-        //     )::date AS month
-        // )
-        // SELECT
-        //     TO_CHAR(all_months.month, 'YYYY-MM') AS month,
-        //     COALESCE(COUNT(users.id), 0) AS registration_count
-        // FROM
-        //     all_months
-        // LEFT JOIN
-        //         (select * from users   left join role_user on users.id = role_user.user_id  where role_user.role_id = 7)
-        //     users ON TO_CHAR(users.created_at , 'YYYY-MM') = TO_CHAR(all_months.month, 'YYYY-MM')
-
-
-        // GROUP BY
-        //     TO_CHAR(all_months.month, 'YYYY-MM')
-        // ORDER BY
-        //     month;
-        // ");
-
         $query = DB::select("
-            WITH RECURSIVE all_months AS (
-                SELECT '" . $this->tahun . "-01-01' AS month
-                UNION ALL
-                SELECT DATE_ADD(month, INTERVAL 1 MONTH)
-                FROM all_months
-                WHERE month < '" . $this->tahun . "-12-01'
-            )
-            SELECT
-                DATE_FORMAT(all_months.month, '%Y-%m') AS month,
-                COALESCE(COUNT(users.id), 0) AS registration_count
-            FROM
-                all_months
-            LEFT JOIN (
-                SELECT users.*
-                FROM users
-                LEFT JOIN role_user ON users.id = role_user.user_id
-                WHERE role_user.role_id = 7
-            ) AS users ON DATE_FORMAT(users.created_at, '%Y-%m') = DATE_FORMAT(all_months.month, '%Y-%m')
-            GROUP BY
-                DATE_FORMAT(all_months.month, '%Y-%m')
-            ORDER BY
-                month;
+        WITH all_months AS (
+            SELECT generate_series(
+                '" . $this->tahun . "-01-01'::date,
+                '" . $this->tahun . "-12-31'::date,
+                interval '1 month'
+            )::date AS month
+        )
+        SELECT
+            TO_CHAR(all_months.month, 'YYYY-MM') AS month,
+            COALESCE(COUNT(users.id), 0) AS registration_count
+        FROM
+            all_months
+        LEFT JOIN
+                (select * from users   left join role_user on users.id = role_user.user_id  where role_user.role_id = 7)
+            users ON TO_CHAR(users.created_at , 'YYYY-MM') = TO_CHAR(all_months.month, 'YYYY-MM')
+
+
+        GROUP BY
+            TO_CHAR(all_months.month, 'YYYY-MM')
+        ORDER BY
+            month;
         ");
+
+        // $query = DB::select("
+        //     WITH RECURSIVE all_months AS (
+        //         SELECT '" . $this->tahun . "-01-01' AS month
+        //         UNION ALL
+        //         SELECT DATE_ADD(month, INTERVAL 1 MONTH)
+        //         FROM all_months
+        //         WHERE month < '" . $this->tahun . "-12-01'
+        //     )
+        //     SELECT
+        //         DATE_FORMAT(all_months.month, '%Y-%m') AS month,
+        //         COALESCE(COUNT(users.id), 0) AS registration_count
+        //     FROM
+        //         all_months
+        //     LEFT JOIN (
+        //         SELECT users.*
+        //         FROM users
+        //         LEFT JOIN role_user ON users.id = role_user.user_id
+        //         WHERE role_user.role_id = 7
+        //     ) AS users ON DATE_FORMAT(users.created_at, '%Y-%m') = DATE_FORMAT(all_months.month, '%Y-%m')
+        //     GROUP BY
+        //         DATE_FORMAT(all_months.month, '%Y-%m')
+        //     ORDER BY
+        //         month;
+        // ");
 
 
         $jingan = collect($query);
